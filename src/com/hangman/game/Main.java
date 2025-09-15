0ebc00234b21eebac5843c4559a11fbe4642b772
Bakyt Sheishenbekov <76763584+bakyt-dev@users.noreply.github.com>
2025-09-15 23:44:59 +0600

    Update Main.java

--- src/com/hangman/game/Main.java
+++ src/com/hangman/game/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         GameController gc = new GameController();
-        gc.initialPlayerChose();
+        gc.initialPlayerChoice();
 
 
     }
